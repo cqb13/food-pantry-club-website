9ce622790d297990d4156aa29e0307958f505fe8
cqb13 <cqb13.main@gmail.com>
2023-11-10 10:44:34 -0500

    Club Info & FAQ & Removed unused files

---
+++ src/components/FaqCard.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{
+  question: string
+  answer: string
+}>()
+
+const isAnswerVisible = ref(false)
+
+const toggleAnswerVisibility = () => {
+  isAnswerVisible.value = !isAnswerVisible.value
+}
+</script>
+
+<template>
+  <section class="bg-secondary bg-opacity-50 rounded-2xl p-2">
+    <div class="flex items-center justify-between">
+      <h3 class="text-xl">{{ question }}</h3>
+      <button @click="toggleAnswerVisibility">
+        <svg
+          fill="#986767"
+          version="1.1"
+          id="Layer_1"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          width="50px"
+          height="50px"
+          viewBox="0 0 100 100"
+          enable-background="new 0 0 100 100"
+          xml:space="preserve"
+          class="transform"
+          :class="{ 'rotate-45': !isAnswerVisible }"
+        >
+          <path
+            d="M84.707,68.752L65.951,49.998l18.75-18.752c0.777-0.777,0.777-2.036,0-2.813L71.566,15.295
+	c-0.777-0.777-2.037-0.777-2.814,0L49.999,34.047l-18.75-18.752c-0.746-0.747-2.067-0.747-2.814,0L15.297,28.431
+	c-0.373,0.373-0.583,0.88-0.583,1.407c0,0.527,0.21,1.034,0.583,1.407L34.05,49.998L15.294,68.753
+	c-0.373,0.374-0.583,0.88-0.583,1.407c0,0.528,0.21,1.035,0.583,1.407l13.136,13.137c0.373,0.373,0.881,0.583,1.41,0.583
+	c0.525,0,1.031-0.21,1.404-0.583l18.755-18.755l18.756,18.754c0.389,0.388,0.896,0.583,1.407,0.583c0.511,0,1.019-0.195,1.408-0.583
+	l13.138-13.137C85.484,70.789,85.484,69.53,84.707,68.752z"
+          />
+        </svg>
+      </button>
+    </div>
+    <div v-if="isAnswerVisible">
+      <p>{{ answer }}</p>
+    </div>
+  </section>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import FaqCard from '@/components/FaqCard.vue'
 //https://via.placeholder.com/350
 </script>
 
@@ -6,42 +7,109 @@
   <main class="px-64 py-10">
     <section class="flex flex-col gap-5">
       <div class="flex gap-5">
-        <img src="/src/assets/logo.png" alt="logo" width="350px" height="350px" class="rounded-3xl rounded-tl-9xl"/>
+        <img
+          src="/src/assets/logo.png"
+          alt="logo"
+          width="350px"
+          height="350px"
+          class="rounded-3xl rounded-tl-9xl"
+        />
         <article class="bg-secondary p-5 rounded-3xl rounded-tr-9xl">
-          <h2>What is the SHS Food Pantry Club?</h2>
-          <hr class="w-3/5">
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse eos rem ad atque eum,
-            voluptatem commodi harum aliquam quisquam et obcaecati odio deleniti debitis quos fuga!
-            Voluptatem dicta magni sequi cupiditate quas, praesentium eligendi! Nemo exercitationem
-            dolor sint ducimus cum, laboriosam expedita nobis dolorem. Eligendi placeat maiores est
-            nisi ipsa, eveniet minus earum, exercitationem omnis, suscipit sint. Quidem ex soluta
-            dignissimos quaerat illo animi atque cum eos quasi temporibus reiciendis, neque culpa
-            dolorum saepe debitis corporis facilis quos perspiciatis ratione velit ipsum non minima
-            sint similique. Reiciendis ipsum quibusdam incidunt libero sunt nulla quis, nobis nisi
-            repellat nihil doloribus cumque.
+          <h2 class="text-2xl">What is the SHS Food Pantry Club?</h2>
+          <hr class="w-3/5" />
+          <p class="text-xl">
+            We are a passionate group of volunteers, focused on tackling food insecurity in the
+            Greater Boston Area. Through food drives and hands-on volunteering, we aim to make a
+            positive impact on local pantries and the lives of those in need. We're looking for
+            dedicated volunteers who are self-motivated and ready to work collaboratively with
+            supervisors. If you're committed to making a difference and want to be part of our
+            mission, we'd love to have you on board.
           </p>
         </article>
       </div>
       <div class="flex gap-5">
         <article class="bg-background-950 bg-opacity-5 p-5 flex-1 rounded-3xl rounded-bl-9xl">
-          <h2>Club Info</h2>
-          <hr class="w-3/5">
-          <p>
-            Stuff about the club here, but in better format (meetings times, etc.)
-          </p>
+          <h2 class="text-2xl">How to get Involved?</h2>
+          <hr class="w-3/5" />
+          <div>
+            <p>Attend our Monday meetings in Room 2304.</p>
+            <ul class="list-disc pl-5">
+              <li>2:45 - 3:15 PM</li>
+            </ul>
+          </div>
+          <div>
+            <p>Join our Schoology group using the provided code.</p>
+            <ul class="list-disc pl-5">
+              <li>Code: 2DN8-6T6X-NCJSB</li>
+            </ul>
+          </div>
         </article>
-        <img src="/src/assets/group-pic.jpeg" alt="group pic" width="400px" height="400px" class="rounded-3xl rounded-br-9xl"/>
+        <img
+          src="/src/assets/group-pic.jpeg"
+          alt="group pic"
+          width="400px"
+          height="400px"
+          class="rounded-3xl rounded-br-9xl"
+        />
       </div>
     </section>
-    <section>
-      <h2>Our Mission</h2>
+
+    <section class="my-10">
+      <h2 class="text-3xl font-bold mb-5">Highlights</h2>
+      <div class="flex gap-5">
+        <img src="{{ placeholderImage }}" alt="Gallery Image 1" class="rounded-lg" />
+        <img src="{{ placeholderImage }}" alt="Gallery Image 2" class="rounded-lg" />
+        <img src="{{ placeholderImage }}" alt="Gallery Image 3" class="rounded-lg" />
+      </div>
     </section>
-    <section>
-      <h2>Our Values</h2>
+
+    <section class="my-10 flex">
+      <div class="w-full">
+        <h2 class="text-3xl font-bold mb-5">FAQ</h2>
+        <p>Answers to some questions you might have.</p>
+      </div>
+      <div class="flex flex-col gap-5 w-full">
+        <FaqCard
+          question="Is the club open to all students?"
+          answer="Yes, our club is open to all students at the school. We welcome diversity and encourage students from all grades to join us in making a positive impact in our community."
+        />
+        <FaqCard
+          question="What volunteer opportunities are available?"
+          answer="We offer various volunteering opportunities, including..."
+        />
+        <FaqCard
+          question="Will I earn volunteer hours?"
+          answer="Volunteer hours can be earned by organizing and participating in events."
+        />
+        <FaqCard
+          question="What benefits do I gain from joining the club?"
+          answer="Joining the club provides you with the opportunity to make a difference in the community, develop leadership skills, and connect with like-minded peers. Additionally, participating in club activities can be a fulfilling and enriching experience."
+        />
+        <FaqCard
+          question="Can I join even if I have no previous volunteer experience?"
+          answer="Absolutely! Our club is inclusive, and no prior volunteer experience is necessary. We value the enthusiasm and dedication of our members, and we provide guidance and support for those new to volunteering."
+        />
+        <FaqCard
+          question="How can I stay updated on club activities and announcements?"
+          answer="To stay informed about club activities, events, and announcements, we recommend joining our Schoology group. We also encourage you to attend our Monday meetings for firsthand information and updates."
+        />
+      </div>
     </section>
-    <section>
-      <h2>Highlights</h2>
+
+    <section class="my-10">
+      <h2 class="text-3xl font-bold mb-5">Contact Information</h2>
+      <p>Email: info@example.com</p>
+      <p>Phone: (123) 456-7890</p>
+    </section>
+
+    <section class="my-10">
+      <h2 class="text-3xl font-bold mb-5">Resources</h2>
+      <p>Explore resources related to food insecurity, volunteering, and community service:</p>
+      <ul class="list-disc pl-5">
+        <li><a href="#">Resource 1</a></li>
+        <li><a href="#">Resource 2</a></li>
+        <li><a href="#">Resource 3</a></li>
+      </ul>
     </section>
   </main>
 </template>
